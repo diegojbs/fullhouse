ad376a97b39a7dde0593d94c2a1a39e208cc04fe
Diego <diego-jbs@hotmail.com>
2018-08-06 12:52:15 -0500

    Se agrega mapa con ubicacion popayan
    Se agrega mapa en el pie de pagina con google maps

--- resources/views/layouts/pie.blade.php
+++ resources/views/layouts/pie.blade.php
@@ -1,4 +1,7 @@
     <!-- pie de pagina -->
+    <h3 class="text-center">¿Cómo llegar?</h3>
+    <hr>
+    <div id="map"></div>
     <div class="container-fluid well">
         <div class="col xs-12 col-sm-4 text-right">
             <h2>Síguenos en las redes sociales</h2>
@@ -21,6 +24,27 @@
    
     <!-- Include all compiled plugins (below), or include individual files as needed -->
     <script src="http://fullhouseprefabricados.com/recursos/js/bootstrap.min.js"></script>
+
+    <script>
+    // Initialize and add the map
+    function initMap() {
+        // The location of fullHousePrefabricados
+        var fullHousePrefabricados = {lat: 2.451456, lng: -76.600789};
+        // The map, centered at fullHousePrefabricados
+        var map = new google.maps.Map(
+            document.getElementById('map'), {zoom: 16, center: fullHousePrefabricados});
+        // The marker, positioned at fullHousePrefabricados
+        var marker = new google.maps.Marker({position: fullHousePrefabricados, map: map, title: 'Full House Prefabricados'});
+    }
+        </script>
+        <!--Load the API from the specified URL
+        * The async attribute allows the browser to render the page while the API loads
+        * The key parameter will contain your own API key (which is not needed for this tutorial)
+        * The callback parameter executes the initMap() function
+        -->
+        <script async defer
+            src="https://maps.googleapis.com/maps/api/js?key={{env("MAP_KEY")}}&callback=initMap">
+        </script>
     
 </body>
 </html>
